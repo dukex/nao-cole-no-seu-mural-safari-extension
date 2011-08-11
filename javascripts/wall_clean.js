a60c500a722b0ec3fc9476707a5275488a0c9bcc
Duke <duke@riseup.net>
2011-08-11 17:31:49 -0300

    improved javascript

--- javascripts/wall_clean.js
+++ javascripts/wall_clean.js
@@ -1,8 +1,21 @@
 duke(document).ready(function () {
+  setInterval(duke.removePost, 2000);
+});
+
+duke.removePost = function () {
   duke("#home_stream li").each(function () {
     post = duke(this);
-    wall_text = post.find(".storyContent .messageBody").text();
-    patt= /cole no seu mural/gi;
-    if(wall_text.match(patt)) post.hide();
+    if(duke.isBadPost(post))
+      duke.trash(post);
   });
-});
+}
+
+duke.isBadPost = function (post) {
+    wall_text = post.find(".storyContent .messageBody").text();
+    pattern = /cole no seu mural|COLE ISSO NO SEU MURAL/gi;
+    return wall_text.match(pattern);
+}
+
+duke.trash = function (item) {
+  item.css("border", "2px solid red")
+}
